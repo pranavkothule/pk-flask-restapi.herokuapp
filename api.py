2a610f17695d51c5deb0c9f543aa8a540969bf3e
pranavrk26 <pranavrk26@gmail.com>
2022-05-14 23:37:43 +0530

    first commit

---
+++ api.py
@@ -0,0 +1,30 @@
+from unittest import result
+from fastapi import requests
+from flask import Flask
+from flask_restx import Api, Resource
+import requests
+import json
+
+app=Flask(__name__)
+api=Api(app)
+
+
+@api.route("/restcountries")
+class restcountries(Resource):
+    def get(self):
+        resp = requests.get('https://restcountries.com/v3.1/region/europe')
+        result=json.loads(resp.text)
+        return result
+
+
+@api.route("/publicapis")
+class publicapis(Resource):
+    def get(self):
+        resp = requests.get('https://api.publicapis.org/entries')
+        result=json.loads(resp.text)
+        return result
+
+
+
+if __name__ =="__main__":
+    app.run()
